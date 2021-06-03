2e32a60023f4e2cb9c0ec979b60b40162ca8389e
gilbertlc <gilbert.lacasandile@gmail.com>
2021-06-03 08:31:55 +0800

    done filter

--- js/main.js
+++ js/main.js
@@ -12,3 +12,36 @@ const bgAnimationItems = () => {
 };
 
 bgAnimationItems();
+
+const filter = document.querySelector(".filter");
+const filterBtns = filter.querySelectorAll(".filter__button");
+
+filterBtns.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("filter__button--active")) {
+      filter
+        .querySelector(".filter__button--active")
+        .classList.remove("filter__button--active");
+      e.target.classList.add("filter__button--active");
+      filterItems(e.target);
+    }
+  });
+});
+
+const filterItems = (filterBtn) => {
+  const selectedCategory = filterBtn.getAttribute("data-filter");
+  document.querySelectorAll(".portfolio-item").forEach((item) => {
+    const category = item.getAttribute("data-category").split(",");
+
+    if (
+      category.indexOf(selectedCategory) !== -1 ||
+      selectedCategory === "all"
+    ) {
+      item.classList.add("portfolio-item--show");
+    } else {
+      item.classList.remove("portfolio-item--show");
+    }
+  });
+};
+
+filterItems(document.querySelector(".filter__button--active"));
