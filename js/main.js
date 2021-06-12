b52c67bae6e71f288efa77426e84355c48ec6b14
gilbertlc <gilbert.lacasandile@gmail.com>
2021-06-12 17:18:23 +0800

    done header & overlay effects

--- js/main.js
+++ js/main.js
@@ -13,6 +13,16 @@ const bgAnimationItems = () => {
 
 bgAnimationItems();
 
+const overlayEffect = () => {
+  for (let x = 0; x < 10; x++) {
+    const div = document.createElement("div");
+    div.className = "overlay-effect__item";
+    document.querySelector(".overlay-effect").appendChild(div);
+  }
+};
+
+overlayEffect();
+
 const toggleBodyScroll = () => {
   document.body.classList.toggle("overflow-y-hidden");
 };
@@ -203,14 +213,14 @@ document.addEventListener("keydown", (e) => {
 });
 
 // smooth scroll for anchor tag starts with #
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(e.target.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     document.querySelector(e.target.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
 
 const toggleContactForm = () => {
   document
@@ -257,3 +267,55 @@ document
       );
     e.target.reset();
   });
+
+const toggleNavbar = () => {
+  document
+    .querySelector(".navbar-toggler")
+    .classList.toggle("navbar-toggler--active");
+  document.querySelector(".navbar").classList.toggle("navbar--open");
+};
+
+const toggleOverlayEffect = () => {
+  document
+    .querySelector(".overlay-effect")
+    .classList.toggle("overlay-effect--active");
+};
+
+document.querySelector(".navbar-toggler").addEventListener("click", () => {
+  toggleNavbar();
+  toggleOverlayEffect();
+});
+
+const toggleSection = (hash) => {
+  // hide section that doesnt have .hidden (shown sections)
+  document.querySelectorAll("section:not(.hidden)").forEach((section) => {
+    section.classList.add("hidden");
+  });
+  document.querySelector(hash).classList.remove("hidden");
+};
+
+document.querySelectorAll('a[href^="#"]').forEach((hashLink) => {
+  hashLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    const hash = e.target.hash;
+
+    if (e.target.classList.contains("navbar__link")) {
+      toggleSection(hash);
+      toggleNavbar();
+      toggleOverlayEffect();
+    } else {
+      document
+        .querySelector(".navbar-toggler")
+        .classList.add("navbar-toggler--hide");
+      toggleOverlayEffect();
+
+      setTimeout(() => {
+        toggleSection(hash);
+        document
+          .querySelector(".navbar-toggler")
+          .classList.remove("navbar-toggler--hide");
+        toggleOverlayEffect();
+      }, 950);
+    }
+  });
+});
