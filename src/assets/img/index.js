0276ebed71ae9d4b1b9bba15cf1d3322375995c2
gilbertlc <gilbert.lacasandile@gmail.com>
2021-07-27 21:13:58 +0800

    add clones

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,9 +1,17 @@
-export default {
+const imgs = {
   archmove1: require("./archmove/1.png").default,
   archmove2: require("./archmove/2.png").default,
   portfolio1: require("./portfolio/1.png").default,
   portfolio2: require("./portfolio/2.png").default,
   portfolio3: require("./portfolio/3.png").default,
+  amazonclone1: require("./amazonclone/1.png").default,
+  amazonclone2: require("./amazonclone/2.png").default,
+  amazonclone3: require("./amazonclone/3.png").default,
+  amazonclone4: require("./amazonclone/4.png").default,
+  netflixclone1: require("./netflixclone/1.png").default,
+  netflixclone2: require("./netflixclone/2.png").default,
+  netflixclone3: require("./netflixclone/3.png").default,
+  netflixclone4: require("./netflixclone/4.png").default,
   diliDashboard: require("./dili/dashboard.png").default,
   diliCertificates: require("./dili/certificates.png").default,
   diliEvents: require("./dili/events.png").default,
@@ -14,3 +22,5 @@ export default {
   sjnhsStudents: require("./sjnhs/students.png").default,
   sjnhsVote: require("./sjnhs/vote.png").default,
 };
+
+export default imgs;
--- src/data/items.js
+++ src/data/items.js
@@ -1,8 +1,7 @@
 import img from "../assets/img";
 
-const items = [
+let items = [
   {
-    id: 1,
     categories: ["html-css"],
     thumbnail: img.archmove1,
     screenshots: [img.archmove1, img.archmove2],
@@ -14,7 +13,6 @@ const items = [
     url: "https://gilbertlc.github.io/archmove/",
   },
   {
-    id: 2,
     categories: ["html-css", "react"],
     thumbnail: img.portfolio1,
     screenshots: [img.portfolio1, img.portfolio2, img.portfolio3],
@@ -22,12 +20,51 @@ const items = [
     description:
       "Built a portfolio website to showcase my recent projects. I've used React, styled and animate it with CSS, and VanillaJS for building modal, slider, pagination, etc. You can check out the link to my source code below.",
     created: "2021",
-    technologies: ["HTML", "CSS (preprocessed w/ SASS)", "JavaScript", "React"],
+    technologies: [
+      "HTML5",
+      "CSS3 (preprocessed w/ SASS)",
+      "Vanilla JavaScript",
+      "React",
+      "Firebase",
+    ],
     role: ["Front-End"],
     url: "https://github.com/gilbertlc/gilbert-portfolio",
   },
   {
-    id: 3,
+    categories: ["react", "full-stack"],
+    thumbnail: img.amazonclone1,
+    screenshots: [
+      img.amazonclone1,
+      img.amazonclone2,
+      img.amazonclone3,
+      img.amazonclone4,
+    ],
+    title: "Amazon Clone",
+    description:
+      "A working simple amazon clone built with react, firebase, and stripe. Inspired from <a href='https://www.youtube.com/watch?v=RDV3Z1KCBvo' target='_blank'>Let's Build a Full-Stack AMAZON Clone with REACT JS for Beginners (Full E-Comm Store in 8 Hrs) 2021</a>.",
+    created: "2021",
+    technologies: ["React", "Firebase", "StripeJS"],
+    role: ["Front-End", "Back-End"],
+    url: "https://clone-1e474.web.app/",
+  },
+  {
+    categories: ["react", "full-stack"],
+    thumbnail: img.netflixclone2,
+    screenshots: [
+      img.netflixclone1,
+      img.netflixclone2,
+      img.netflixclone3,
+      img.netflixclone4,
+    ],
+    title: "Netflix Clone",
+    description:
+      "A simple netflix clone built with react, firebase, and stripe. Inspired from <a href='https://www.youtube.com/watch?v=CLMo0W7mTVo&list=PLf16UKl7nR5DaxECWIwHr82oXtQWgI4Ri' target='_blank'>NETFLIX REACT.JS 5-Day Challenge</a>.",
+    created: "2021",
+    technologies: ["React", "Firebase", "StripeJS"],
+    role: ["Front-End", "Back-End"],
+    url: "https://netflixclone-d2648.web.app/",
+  },
+  {
     categories: ["bootstrap", "full-stack"],
     thumbnail: img.diliDashboard,
     screenshots: [
@@ -40,11 +77,10 @@ const items = [
     description:
       "DMS is a system for collecting, sorting, retrieving and processing information of the residents in barangay which is used, or desired, by one of more managers, in the performance of their duties. This is our thesis back in college, we were consist of 3 individuals and I am the lead developer and designer of this project.",
     created: "2017",
-    technologies: ["HTML", "CSS", "jQuery", "Bootstrap", "Django", "MySQL"],
+    technologies: ["jQuery", "Bootstrap", "Django", "MySQL"],
     role: ["Front-End", "Back-End"],
   },
   {
-    id: 4,
     categories: ["bootstrap", "full-stack"],
     thumbnail: img.sjnhsLogin,
     screenshots: [
@@ -58,9 +94,11 @@ const items = [
     description:
       "Built a voting system for a local secondary school for a client's thesis as well as help them write a documentation for it. I used Bootstrap for styling and PHP in the back-end.",
     created: "2017",
-    technologies: ["HTML", "CSS", "jQuery", "Bootstrap", "PHP", "MySQL"],
+    technologies: ["jQuery", "Bootstrap", "PHP", "MySQL"],
     role: ["Front-End", "Back-End"],
   },
 ];
+
+items = items.map((item, index) => ({...item, id: index}));
 
 export default items;
