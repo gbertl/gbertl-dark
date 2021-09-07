82530fa2121bf8882f31d629efedeeecf35fcfea
gilbertlc <gilbert.lacasandile@gmail.com>
2021-09-07 21:31:12 +0800

    fix devtools for prod

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,10 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : (x) => x;
+
+const store = createStore(reducers, compose(applyMiddleware(thunk), devTools));
 
 export default store;
