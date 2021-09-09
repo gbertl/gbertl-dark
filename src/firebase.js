64ae71e0bfeb4bb074114075dc3b41e9296d2fd7
gilbertlc <gilbert.lacasandile@gmail.com>
2021-09-09 20:26:34 +0800

    rename analytics -> firebaseAnalytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1nuICRTKZh-QBSG5g-zrizy5QIijdGY8",
-  authDomain: "codergilbert.firebaseapp.com",
-  projectId: "codergilbert",
-  storageBucket: "codergilbert.appspot.com",
-  messagingSenderId: "790516466440",
-  appId: "1:790516466440:web:cabb6bc1e1d98ad4b9e2bd",
-  measurementId: "G-GQHM92W74Z",
+  apiKey: 'AIzaSyA1nuICRTKZh-QBSG5g-zrizy5QIijdGY8',
+  authDomain: 'codergilbert.firebaseapp.com',
+  projectId: 'codergilbert',
+  storageBucket: 'codergilbert.appspot.com',
+  messagingSenderId: '790516466440',
+  appId: '1:790516466440:web:cabb6bc1e1d98ad4b9e2bd',
+  measurementId: 'G-GQHM92W74Z',
 };
 
 firebase.initializeApp(firebaseConfig);
-export const analytics = firebase.analytics();
+
+export const firebaseAnalytics = firebase.analytics();
