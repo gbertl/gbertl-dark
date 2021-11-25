c12cc73931adeae991b564ac3d5feeee1e8867ba
gilbertlc <gilbert.lacasandile@gmail.com>
2021-11-25 22:12:08 +0800

    use getStaticProps and revalidate in 3 days for /portfolio

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,11 +2,12 @@ import Portfolio from '../components/Portfolio';
 import { wrapper } from '../store';
 import { fetchCategories, fetchProjects } from '../store/actions/portfolio';
 
-export const getServerSideProps = wrapper.getServerSideProps(
+export const getStaticProps = wrapper.getStaticProps(
   ({ dispatch }) =>
     async () => {
       await dispatch(fetchProjects());
       await dispatch(fetchCategories());
+      return { revalidate: 259200 };
     }
 );
 
