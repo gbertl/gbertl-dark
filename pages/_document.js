71f57d82432a72818e313c916a95a0ce16fac95d
gilbertlc <gilbert.lacasandile@gmail.com>
2021-11-25 15:11:33 +0800

    fix typo referrerPolicy

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-            referrerpolicy="no-referrer"
+            referrerPolicy="no-referrer"
           />
         </Head>
         <body>
