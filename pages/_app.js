e488dea8b04a4fdf4a8cc8881270475b3333ac28
gilbertlc <gilbert.lacasandile@gmail.com>
2022-01-24 11:10:50 +0800

    setup analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,31 @@
 import '../styles/scss/style.scss';
 import { wrapper } from '../store';
 import Layout from '../components/Layout';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+          />
+          <Script id="analytics-script">
+            {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
+              `}
+          </Script>
+        </>
+      )}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
