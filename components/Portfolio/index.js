755e08181e4886d0654889dab900e45e43331f68
gilbertlc <gilbert.lacasandile@gmail.com>
2022-02-20 14:57:43 +0800

    Revert "use btn-secondary in filter__button"
    This reverts commit 22ce8098e2c587edea6ac59d6f46e75ad4a26b34.

--- components/Portfolio/index.js
+++ components/Portfolio/index.js
@@ -40,8 +40,8 @@ const Portfolio = () => {
             <li>
               <button
                 onClick={() => handleFilter('all')}
-                className={`btn-secondary${
-                  filterTitle === 'All' ? ' btn-secondary--active' : ''
+                className={`filter__button${
+                  filterTitle === 'All' ? ' filter__button--active' : ''
                 }`}
               >
                 All
@@ -51,8 +51,8 @@ const Portfolio = () => {
               <li key={c.name}>
                 <button
                   onClick={() => handleFilter(c.name)}
-                  className={`btn-secondary${
-                    filterTitle === c.title ? ' btn-secondary--active' : ''
+                  className={`filter__button${
+                    filterTitle === c.title ? ' filter__button--active' : ''
                   }`}
                   title={`Filter By ${c.title}`}
                 >
