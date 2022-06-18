07c4341f03f50a3f4e654cfc242f7f36ebadf158
Gilbert L <gilbertlcsndle@gmail.com>
2022-06-18 22:49:19 +0800

    reformat loading spinner

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -6,32 +6,12 @@ import { selectProjects } from '../store/slices/portfolio';
 
 const Loader = () => {
   const isLoading = useSelector(selectIsLoading);
-  const [sec, setSec] = useState(8);
-  const countDownRef = useRef(0);
 
   const areProjectsFetched = useSelector(selectProjects).length;
   const dispatch = useDispatch();
 
   useEffect(() => {
-    countDownRef.current = window.setInterval(() => {
-      setSec((prevSec) => {
-        if (prevSec > 0) {
-          return prevSec - 1;
-        } else {
-          clearInterval(countDownRef.current);
-          return 0;
-        }
-      });
-    }, 1000);
-
-    return () => {
-      clearInterval(countDownRef.current);
-    };
-  }, []);
-
-  useEffect(() => {
     if (isLoading && areProjectsFetched) {
-      clearInterval(countDownRef.current);
       dispatch(hideLoader());
     }
   }, [isLoading, areProjectsFetched]);
@@ -40,9 +20,10 @@ const Loader = () => {
     <div className="loader">
       <div className="loader__container">
         <span className="spinner"></span>
-        <h2>Waking up my API from Heroku&apos;s free tier.</h2>
+        <h2>Please wait for my API to boot up.</h2>
         <p>
-          Please allow up to {sec} second{sec > 1 ? 's' : ''}...
+          I only use free web hosting for my API so it sleeps after 30 minutes
+          of inactivity.
         </p>
       </div>
     </div>
